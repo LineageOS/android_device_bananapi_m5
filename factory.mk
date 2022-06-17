349b60b01b9ae0956130056f546809f14ae817e8
Nolen Johnson <johnsonnolen@gmail.com>
2022-06-17 17:13:40 -0400

    m5: factory: Move u-boot somewhere accessible
    Change-Id: Ifa1f6eb8f4830e7b23f98bedced4eedbf1c8a435

--- factory.mk
+++ factory.mk
@@ -37,8 +37,8 @@ $(INSTALLED_AML_UPGRADE_PACKAGE_TARGET): $(addprefix $(PRODUCT_OUT)/,$(NEEDED_IM
 	$(hide) mkdir -p $(PRODUCT_UPGRADE_OUT)
 ifneq ("$(wildcard $(FACTORY_PATH)/u-boot.bin)","")
 	$(hide) $(call aml-copy-file, $(FACTORY_PATH)/u-boot.bin)
-else ifneq ("$(wildcard vendor/firmware/m5/radio/bootloader.img)","")
-	$(hide) $(call aml-copy-file, vendor/firmware/m5/radio/bootloader.img, u-boot.bin)
+else ifneq ("$(wildcard vendor/amlogic/m5/radio/bootloader.img)","")
+	$(hide) $(call aml-copy-file, vendor/amlogic/m5/radio/bootloader.img, u-boot.bin)
 else
 	$(error "no u-boot.bin found in $(FACTORY_PATH)")
 endif
