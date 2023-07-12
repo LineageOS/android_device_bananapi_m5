eac91bde8b15ecb9778211de7fa1747d078ffccf
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-12 17:30:28 -0400

    m5: Allow tablet builds
    Change-Id: Ib400d6c6d87e4d663335f2fb73b47297462561a6

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m5.mk
+    $(LOCAL_DIR)/lineage_m5.mk \
+    $(LOCAL_DIR)/lineage_m5_tab.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_m5-eng \
+    lineage_m5_tab-eng \
     lineage_m5-user \
-    lineage_m5-userdebug
+    lineage_m5_tab-user \
+    lineage_m5-userdebug \
+    lineage_m5_tab-userdebug
