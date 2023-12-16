ad581904209f559ca67f3b011c464b1d95f3f915
Bruno Martins <bgcngm@gmail.com>
2023-12-16 21:54:10 +0000

    m5: Properly build BT and WLAN kernel modules
    Change-Id: I1b02fd710c43ef66025d5d395728785c0ce3ddbe
    (cherry picked from commit 52b262a87eebc2e5a32571b8724fa900a6b339e7)

--- device.mk
+++ device.mk
@@ -16,11 +16,6 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
 
-## Kernel Modules
-PRODUCT_PACKAGES += \
-    8822cs \
-    rtk_btusb
-
 ## Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
