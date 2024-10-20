085a60ab72e055f58f01a506cda51db053ae76c1
Bruno Martins <bgcngm@gmail.com>
2024-10-20 11:35:53 +0100

    m5: Add a shim extract that extracts only common tree
    * Just a bootloader image that we build out of tree, so
      extraction makes no sense. Just handle firmware @
      `${DEVICE_PATH}/Android.mk`.
    * Import bootloader/misc images.
    * We must reset LOCAL_PATH as the wpa_supplicant include
      forces it, and call-my-dir can't be used after any
      one makefile is included.
    
    Change-Id: I1e362296e937306f059da6e4e0c240aebc2a7921

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,3 @@ PRODUCT_CFI_INCLUDE_PATHS += hardware/realtek/wlan/wpa_supplicant_8_lib
 
 ## Include the common tree BoardConfig makefile
 include device/amlogic/sm1-common/BoardConfigCommon.mk
-
-## Include the proprietary BoardConfig makefile
-include vendor/amlogic/m5/BoardConfigVendor.mk
