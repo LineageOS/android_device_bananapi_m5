0c26ae44ed66d7379d4c3dcd56a791a5c8137765
Bruno Martins <bgcngm@gmail.com>
2024-10-20 11:35:53 +0100

    m5: Add a shim extract that extracts only common tree
    * Just a bootloader image that we build out of tree, so
      extraction makes no sense. Just handle firmware @
      `${DEVICE_PATH}/Android.mk`.
    * Import bootloader/misc images.
    * We must reset LOCAL_PATH as the wpa_supplicant include
      forces it.
    
    Change-Id: I1e362296e937306f059da6e4e0c240aebc2a7921

--- Android.mk
+++ Android.mk
@@ -6,13 +6,15 @@
 
 ifneq ($(filter m5 m5_tab, $(TARGET_DEVICE)),)
 
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := device/bananapi/m5
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-include $(LOCAL_PATH)/factory.mk
 
 include external/wpa_supplicant_8/wpa_supplicant/wpa_supplicant_conf.mk
 
--include vendor/amlogic/m5/firmware.mk
+RADIO_FILES := $(wildcard $(LOCAL_PATH)/factory/bootfiles/*)
+$(foreach f, $(notdir $(RADIO_FILES)), \
+    $(call add-radio-file,factory/bootfiles/$(f)))
+
+include $(LOCAL_PATH)/factory.mk
 
 endif
