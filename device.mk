877d2695983718400a8b55e1ff24ccafd89bb3c4
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-28 02:16:32 -0500

    m5: Drop extract setup
    * We used to use this for more, but now there are 0 files.
    * Just a bootloader image that we build out of tree, so
      extraction makes no sense. Just hand create firmware.mk @
      `vendor/amlogic/${DEVICE}/firmware.mk`, store bootloader @
      `endor/amlogic/${DEVICE}/radio/bootloader.img`.
    
    Change-Id: I56cc92329e15a50a86da2239b91822aee59e8d2b

--- device.mk
+++ device.mk
@@ -33,6 +33,3 @@ TARGET_HAS_TEE := false
 
 ## Inherit from the common tree product makefile
 $(call inherit-product, device/amlogic/sm1-common/sm1.mk)
-
-## Inherit from the proprietary files makefile
-$(call inherit-product, vendor/amlogic/m5/m5-vendor.mk)
