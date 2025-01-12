9ce227a424c9a4ac5d7d9410cf9e54ab4a69de02
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-11 23:40:31 -0500

    m5: Drop wpa_supplicant include
    Change-Id: I6414f9cb1d26371192bc85ab2e0c1932cc028243

--- Android.mk
+++ Android.mk
@@ -6,8 +6,6 @@
 
 ifneq ($(filter m5 m5_tab, $(TARGET_DEVICE)),)
 
-include external/wpa_supplicant_8/wpa_supplicant/wpa_supplicant_conf.mk
-
 LOCAL_PATH := device/bananapi/m5
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
